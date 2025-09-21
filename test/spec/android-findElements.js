3c821b1efdd4ad9313be4e92fb7a5ea788931c1b
Rishikesh-pal <rishikeshpal203@gmail.com>
2025-09-21 10:09:36 +0530

    added more tests

--- test/spec/android-findElements.js
+++ test/spec/android-findElements.js
@@ -6,18 +6,19 @@ describe('Android Elements', async () => {
 
     it('Find element by class name', async () => {
         const className = await $$('android.widget.TextView');
-
-        console.log(className.getText());
-
-        expect(className).toHaveText("API Demos")
+        expect(className).toHaveText("API Demos");
     });
 
-    it('Find elements using XPATH', async () => {
-        // await $('(//android.widget.TextView)[3]').click();
+    xit('Find elements using XPATH', async () => {
         await $('//android.widget.TextView[@content-desc="Alert Dialogs"]').click();
         await $('//android.widget.Button[@resource-id="io.appium.android.apis:id/select_button"]').click();
-        await $('//android.widget.TextView(@text="Command two")');
-        await expect($('//android.widget.TextView')).toHaveText('You selected: 1 , Command two');
+        await $('//android.widget.TextView[@text="Command two"]');
+        const textAssertion = await $('//android.widget.TextView');
+        await expect(textAssertion).toHaveText("You selected: 1 , Command two");
 
-    })
-})
+    });
+
+    it('Find element by uiautomator', async () => {
+        await $('android=new UiSelector().textContains("Alert")').click();
+    });
+});
